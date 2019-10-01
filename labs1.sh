52cf7a2ac41fbcb2b4a437cc4c056237f40ba066
VIacheslav <55996931+liporka@users.noreply.github.com>
2019-10-02 00:36:29 +0200

    Add files via upload

---
+++ labs1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+rm -f ./result.xls
+IFS=$'\n'
+printf "Название файла\tРасширение файла\tДата Изменения\tРазмер\tДлительность\tДлительность\n" >> result.xls
+start=$(pwd)
+
+function file_size() {
+	bytes=$(wc -c < $name)
+	let "kbytes = "$bytes" / 1024"
+	echo "$kbytes"
+}
+
+function mainfnc() {
+	for file in $(ls); do
+ 		if [[ $(basename "$file") = result.xls ]]; then
+ 			continue
+ 		fi
+		echo "found \"$file\""
+		if [[ -d "$file" ]]; then
+			cd "$file"
+			mainfnc "$file"
+			cd ..
+		fi
+		if [[ -f "$file" ]]; then
+			name=$(basename "$file")
+			extension=${name#*.}
+			file_size=$(file_size)
+			last_edited=$(date -r "$file" +%T" "%d/%m/%y)
+ 			if file -ib "$file" | grep -qE 'video|audio'; then
+			 	duration=$(ffmpeg -i $file 2>&1 | grep Duration | awk '{print $2}')	
+		 	else	
+				duration="-"
+			fi
+ 			filepath=$(pwd)
+		 	cd "${start}"
+			printf "${name%%.*}\t$extension\t$last_edited\t$file_size KB\t$filepath\t$duration\n" >> result.xls
+			cd "${filepath}"
+		fi
+	done
+}
+
+mainfnc "$start"
+echo "Done!"
